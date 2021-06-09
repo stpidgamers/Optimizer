6fdb10cefbcb3d40cb94fdb71d5355505c59f529
Vescure <summerclientnene@gmail.com>
2021-06-09 08:03:35 +0800

    ok idk doing shit

---
+++ src/club/vescure/Client.java
@@ -0,0 +1,8 @@
+package club.vescure;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/club/vescure/Main.java
+++ src/club/vescure/Main.java
@@ -1,6 +1,8 @@
+package club.vescure;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Client();
     }
 }
